36340d95e34c55b7c61a167bc352f56c5d55bf36
Bash259 <braimohbabajide259@gmail.com>
2021-07-23 03:50:54 -0400

    2nd commit

--- src/main/java/ucf/assignments/MainAppController.java
+++ src/main/java/ucf/assignments/MainAppController.java
@@ -5,36 +5,21 @@ import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Group;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.paint.Color;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
-import javax.swing.text.html.ImageView;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.net.URL;
-import java.nio.file.attribute.FileTime;
-import java.util.Locale;
-import java.util.Objects;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
-import java.util.logging.Logger;
 
 import static javafx.application.Application.launch;
-import static org.testng.reporters.jq.BasePanel.C;
 
 public class MainAppController implements Initializable {
     @FXML
@@ -45,34 +30,25 @@ public class MainAppController implements Initializable {
     public TextField ItemName;
     @FXML
     public TextField FilterTable;
-
+    @FXML
     public TableView<Item> PrintTable;
     public TableColumn<Item,String> ItemValueList;
     public TableColumn<Item,String> ItemSerialList;
     public TableColumn<Item,String> ItemNameList;
 
     private final ObservableList<Item> list = FXCollections.observableArrayList();
-    public Button AddItem;
-    public Button buttonRemove;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         ItemValueList.setCellValueFactory(new PropertyValueFactory<>("ItemValue"));
         ItemSerialList.setCellValueFactory(new PropertyValueFactory<>("ItemSerial"));
         ItemNameList.setCellValueFactory(new PropertyValueFactory<>("ItemName"));
-
         PrintTable.setEditable(true);
+
         ItemValueList.setCellFactory(TextFieldTableCell.forTableColumn());
         ItemSerialList.setCellFactory(TextFieldTableCell.forTableColumn());
         ItemNameList.setCellFactory(TextFieldTableCell.forTableColumn());
 
-
-        Item item = new Item("Pog","ABCDE23J","Xbox");
-        Item item1 = new Item("Soy","LOLSUSY","CaLin");
-        Item item2 = new Item("Boy","BALSOY","Zoom");
-        Item item3 = new Item("Toy","LULSUS","Damn");
-
-        list.addAll(item,item1,item2,item3);
         PrintTable.setItems(list);
 
         FilteredList<Item> filteredList = new FilteredList<>(list, b->true);
@@ -97,26 +73,48 @@ public class MainAppController implements Initializable {
 
         sortedList.comparatorProperty().bind(PrintTable.comparatorProperty());
         PrintTable.setItems(sortedList);
-    }
-
-    public void buttonRemove(ActionEvent actionEvent) {
-        ObservableList<Item> allProduct,SingleProduct;
-        allProduct = PrintTable.getItems();
-        SingleProduct = PrintTable.getSelectionModel().getSelectedItems();
-        SingleProduct.forEach(allProduct::remove);
-    }
-
-    public void onEditChanged(TableColumn.CellEditEvent cellEditEvent) {
 
     }
 
-    public void PrintTableTable(SortEvent<TableView> tableViewSortEvent) {
-
-    }
 
     public void AddItem(ActionEvent event) {
-        
-        Item item = new Item(ItemValue.getText(),ItemSerialList.getText(),ItemName.getText());
-        PrintTable.getItems().add(item);
+        Item item = new Item("$"+ ItemValue.getText() , ItemNumber.getText(),ItemName.getText());
+        list.add(item);
+    }
+
+    public void onEditChanged(TableColumn.CellEditEvent<Item, String> itemStringCellEditEvent) {
+    }
+
+    public void buttonRemove(javafx.event.ActionEvent actionEvent) {
+        int number = PrintTable.getSelectionModel().getSelectedIndex();
+        list.remove(number);
+    }
+
+    public void BackToList(ActionEvent event) {
+
+    }
+
+    public void clearList(ActionEvent event) {
+        ObservableList<Item> allProduct,SingleProduct;
+        list.clear();
+    }
+
+    public void PrintList(ActionEvent event) {
+        int counter ;
+        ArrayList<String> listSaver = new ArrayList<>();
+        for (counter = 0;counter < list.size();counter++){
+            String Output = list.get(counter).getItemValue() +" "+ list.get(counter).getItemSerial() +" "+list.get(counter).getItemName();
+            listSaver.add(Output);
+            System.out.println(Output);
+        }
+        FileChooser fileChooser = new FileChooser();
+        Stage primaryStage = new Stage();
+        //Set extension filter for text files
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        //Show save file dialog
+        File file = fileChooser.showSaveDialog(primaryStage);
+        primaryStage.show();
     }
 }
